7667208d33937a4acd18b2c6da06d4229b831da0
edearth <nepormoseno1992@gmail.com>
2020-05-09 23:30:54 +0200

    Fix exiting dmenu clearing clipboard

--- Scripts/select-emoji.sh
+++ Scripts/select-emoji.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-cat ~/Scripts/emojis-unicode | rofi -dmenu -i -p 'Select emoji' -l 10 -fn Monospace-16 -theme "~/.config/rofi/onedark.rasi" | cut -f1 -d' ' | tr -d '\n' | xclip -selection clipboard
-
-notify-send "$(xclip -o -selection clipboard) copied to clipboard."
+if emoji=$(cat ~/Scripts/emojis-unicode | rofi -dmenu -i -p 'Select emoji' -l 10 -fn Monospace-16 -theme "~/.config/rofi/onedark.rasi") ; then
+	echo "$emoji" | cut -f1 -d' ' | tr -d '\n' | xclip -selection clipboard
+	notify-send "$(xclip -o -selection clipboard) copied to clipboard."
+fi
 
