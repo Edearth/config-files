03439269286b791c730120a93e8b1fdf58e81c23
edearth <nepormoseno1992@gmail.com>
2020-05-10 05:21:39 +0200

    Use shift+Insert instead of ctrl+v to copy emoji

--- Scripts/select-emoji.sh
+++ Scripts/select-emoji.sh
@@ -4,6 +4,6 @@ if emoji=$(cat ~/Scripts/emojis-unicode | rofi -dmenu -i -p 'Select emoji' -l 10
 	old_clipboard="$(xclip -o -selection clipboard)"
 	echo "$emoji" | cut -f1 -d' ' | tr -d '\n' | xclip -selection clipboard
 	#notify-send "$(xclip -o -selection clipboard) copied to clipboard."
-	which xdotool > /dev/null && xdotool key ctrl+v && echo -n "$old_clipboard" | xclip -selection clipboard
+	which xdotool > /dev/null && xdotool key shift+Insert && echo -n "$old_clipboard" | xclip -selection clipboard
 fi
 
