bb9069f791f9a0f473d0d875db2b60d096bbb243
edearth <nepormoseno1992@gmail.com>
2020-05-10 05:04:51 +0200

    Insert emoji instead of just copying to clipboard
    Putting the emoji in the clipboard is still in the script,
    just commented out.

--- Scripts/select-emoji.sh
+++ Scripts/select-emoji.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if emoji=$(cat ~/Scripts/emojis-unicode | rofi -dmenu -i -p 'Select emoji' -l 10 -fn Monospace-16 -theme "~/.config/rofi/onedark.rasi") ; then
+	old_clipboard="$(xclip -o -selection clipboard)"
 	echo "$emoji" | cut -f1 -d' ' | tr -d '\n' | xclip -selection clipboard
-	notify-send "$(xclip -o -selection clipboard) copied to clipboard."
+	#notify-send "$(xclip -o -selection clipboard) copied to clipboard."
+	which xdotool > /dev/null && xdotool key ctrl+v && echo -n "$old_clipboard" | xclip -selection clipboard
 fi
 
